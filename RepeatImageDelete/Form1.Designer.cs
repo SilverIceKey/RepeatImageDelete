078e324cbcdeb14807bb40e7a42ded2e15260d13
SilverIceKey <z516798599@gmail.com>
2020-03-02 14:23:08 +0800

    first upload

---
+++ RepeatImageDelete/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace RepeatImageDelete
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.selectImageFolder = new System.Windows.Forms.Button();
+            this.qqDefault = new System.Windows.Forms.Button();
+            this.originImages = new System.Windows.Forms.ListBox();
+            this.repeatImages = new System.Windows.Forms.ListBox();
+            this.originImage = new System.Windows.Forms.PictureBox();
+            this.repeatImage = new System.Windows.Forms.PictureBox();
+            this.deleteSelected = new System.Windows.Forms.Button();
+            this.deleteAll = new System.Windows.Forms.Button();
+            this.repeatNumLabel = new System.Windows.Forms.Label();
+            this.deleteProgressBar = new System.Windows.Forms.ProgressBar();
+            this.refreshCurrent = new System.Windows.Forms.Button();
+            this.currentFileRepeat = new System.Windows.Forms.Label();
+            this.next = new System.Windows.Forms.Button();
+            this.deleteSelectedAll = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.originImage)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repeatImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // selectImageFolder
+            // 
+            this.selectImageFolder.Location = new System.Drawing.Point(13, 13);
+            this.selectImageFolder.Name = "selectImageFolder";
+            this.selectImageFolder.Size = new System.Drawing.Size(126, 23);
+            this.selectImageFolder.TabIndex = 0;
+            this.selectImageFolder.Text = "选择图片文件夹";
+            this.selectImageFolder.UseVisualStyleBackColor = true;
+            this.selectImageFolder.Click += new System.EventHandler(this.selectImageFolder_Click);
+            // 
+            // qqDefault
+            // 
+            this.qqDefault.Location = new System.Drawing.Point(146, 12);
+            this.qqDefault.Name = "qqDefault";
+            this.qqDefault.Size = new System.Drawing.Size(104, 23);
+            this.qqDefault.TabIndex = 1;
+            this.qqDefault.Text = "qq默认文件夹";
+            this.qqDefault.UseVisualStyleBackColor = true;
+            this.qqDefault.Click += new System.EventHandler(this.qqDefault_Click);
+            // 
+            // originImages
+            // 
+            this.originImages.FormattingEnabled = true;
+            this.originImages.ItemHeight = 12;
+            this.originImages.Location = new System.Drawing.Point(12, 247);
+            this.originImages.Name = "originImages";
+            this.originImages.Size = new System.Drawing.Size(434, 268);
+            this.originImages.TabIndex = 2;
+            this.originImages.SelectedIndexChanged += new System.EventHandler(this.originImages_SelectedIndexChanged);
+            // 
+            // repeatImages
+            // 
+            this.repeatImages.FormattingEnabled = true;
+            this.repeatImages.ItemHeight = 12;
+            this.repeatImages.Location = new System.Drawing.Point(453, 247);
+            this.repeatImages.Name = "repeatImages";
+            this.repeatImages.Size = new System.Drawing.Size(440, 268);
+            this.repeatImages.TabIndex = 3;
+            // 
+            // originImage
+            // 
+            this.originImage.Location = new System.Drawing.Point(13, 43);
+            this.originImage.Name = "originImage";
+            this.originImage.Size = new System.Drawing.Size(433, 198);
+            this.originImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.originImage.TabIndex = 4;
+            this.originImage.TabStop = false;
+            // 
+            // repeatImage
+            // 
+            this.repeatImage.Location = new System.Drawing.Point(453, 43);
+            this.repeatImage.Name = "repeatImage";
+            this.repeatImage.Size = new System.Drawing.Size(440, 198);
+            this.repeatImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.repeatImage.TabIndex = 5;
+            this.repeatImage.TabStop = false;
+            // 
+            // deleteSelected
+            // 
+            this.deleteSelected.Location = new System.Drawing.Point(257, 11);
+            this.deleteSelected.Name = "deleteSelected";
+            this.deleteSelected.Size = new System.Drawing.Size(104, 23);
+            this.deleteSelected.TabIndex = 6;
+            this.deleteSelected.Text = "删除选中";
+            this.deleteSelected.UseVisualStyleBackColor = true;
+            this.deleteSelected.Click += new System.EventHandler(this.deleteSelected_Click);
+            // 
+            // deleteAll
+            // 
+            this.deleteAll.Location = new System.Drawing.Point(541, 11);
+            this.deleteAll.Name = "deleteAll";
+            this.deleteAll.Size = new System.Drawing.Size(133, 23);
+            this.deleteAll.TabIndex = 7;
+            this.deleteAll.Text = "删除所有";
+            this.deleteAll.UseVisualStyleBackColor = true;
+            this.deleteAll.Click += new System.EventHandler(this.deleteAll_Click);
+            // 
+            // repeatNumLabel
+            // 
+            this.repeatNumLabel.AutoSize = true;
+            this.repeatNumLabel.Location = new System.Drawing.Point(453, 522);
+            this.repeatNumLabel.Name = "repeatNumLabel";
+            this.repeatNumLabel.Size = new System.Drawing.Size(95, 12);
+            this.repeatNumLabel.TabIndex = 8;
+            this.repeatNumLabel.Text = "重复图片数量：0";
+            // 
+            // deleteProgressBar
+            // 
+            this.deleteProgressBar.Location = new System.Drawing.Point(453, 538);
+            this.deleteProgressBar.Name = "deleteProgressBar";
+            this.deleteProgressBar.Size = new System.Drawing.Size(440, 23);
+            this.deleteProgressBar.TabIndex = 9;
+            // 
+            // refreshCurrent
+            // 
+            this.refreshCurrent.Location = new System.Drawing.Point(680, 11);
+            this.refreshCurrent.Name = "refreshCurrent";
+            this.refreshCurrent.Size = new System.Drawing.Size(132, 23);
+            this.refreshCurrent.TabIndex = 10;
+            this.refreshCurrent.Text = "刷新当前文件夹";
+            this.refreshCurrent.UseVisualStyleBackColor = true;
+            this.refreshCurrent.Click += new System.EventHandler(this.refreshCurrent_Click);
+            // 
+            // currentFileRepeat
+            // 
+            this.currentFileRepeat.AutoSize = true;
+            this.currentFileRepeat.Location = new System.Drawing.Point(12, 522);
+            this.currentFileRepeat.Name = "currentFileRepeat";
+            this.currentFileRepeat.Size = new System.Drawing.Size(119, 12);
+            this.currentFileRepeat.TabIndex = 11;
+            this.currentFileRepeat.Text = "当前图片重复数量：0";
+            // 
+            // next
+            // 
+            this.next.Location = new System.Drawing.Point(818, 11);
+            this.next.Name = "next";
+            this.next.Size = new System.Drawing.Size(75, 23);
+            this.next.TabIndex = 12;
+            this.next.Text = "下一张";
+            this.next.UseVisualStyleBackColor = true;
+            this.next.Click += new System.EventHandler(this.next_Click);
+            // 
+            // deleteSelectedAll
+            // 
+            this.deleteSelectedAll.Location = new System.Drawing.Point(368, 11);
+            this.deleteSelectedAll.Name = "deleteSelectedAll";
+            this.deleteSelectedAll.Size = new System.Drawing.Size(167, 23);
+            this.deleteSelectedAll.TabIndex = 13;
+            this.deleteSelectedAll.Text = "删除当前图片所有重复";
+            this.deleteSelectedAll.UseVisualStyleBackColor = true;
+            this.deleteSelectedAll.Click += new System.EventHandler(this.deleteSelectedAll_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 573);
+            this.Controls.Add(this.deleteSelectedAll);
+            this.Controls.Add(this.next);
+            this.Controls.Add(this.currentFileRepeat);
+            this.Controls.Add(this.refreshCurrent);
+            this.Controls.Add(this.deleteProgressBar);
+            this.Controls.Add(this.repeatNumLabel);
+            this.Controls.Add(this.deleteAll);
+            this.Controls.Add(this.deleteSelected);
+            this.Controls.Add(this.repeatImage);
+            this.Controls.Add(this.originImage);
+            this.Controls.Add(this.repeatImages);
+            this.Controls.Add(this.originImages);
+            this.Controls.Add(this.qqDefault);
+            this.Controls.Add(this.selectImageFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.originImage)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.repeatImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button selectImageFolder;
+        private System.Windows.Forms.Button qqDefault;
+        private System.Windows.Forms.ListBox originImages;
+        private System.Windows.Forms.ListBox repeatImages;
+        private System.Windows.Forms.PictureBox originImage;
+        private System.Windows.Forms.PictureBox repeatImage;
+        private System.Windows.Forms.Button deleteSelected;
+        private System.Windows.Forms.Button deleteAll;
+        private System.Windows.Forms.Label repeatNumLabel;
+        private System.Windows.Forms.ProgressBar deleteProgressBar;
+        private System.Windows.Forms.Button refreshCurrent;
+        private System.Windows.Forms.Label currentFileRepeat;
+        private System.Windows.Forms.Button next;
+        private System.Windows.Forms.Button deleteSelectedAll;
+    }
+}
+
---
+++ RepeatImageDelete/Form1.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Security.Cryptography;
+
+namespace RepeatImageDelete
+{
+    public partial class Form1 : Form
+    {
+        SynchronizationContext _syncContext = null;
+        private string folderPath;
+        Dictionary<string, string> originImagesList;
+        Dictionary<string, string> originImagesVKList;
+        Dictionary<string, List<string>> repeatImageList;
+        List<object> selectedRepeatItem;
+        int selectedRepeatItemIndex = 0;
+        private int repeatNum = 0;
+        int currentSelectedRepeatNum = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            _syncContext = SynchronizationContext.Current;
+            checkButtonStatus("");
+        }
+
+        private void selectImageFolder_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件夹";
+            if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                originImages.Items.Clear();
+                repeatImages.Items.Clear();
+                folderPath = dialog.SelectedPath;
+                deleteProgressBar.Value = 0;
+                var task1 = new Task(() =>
+                {
+                    repeatImageList = new Dictionary<string, List<string>>();
+                    originImagesList = new Dictionary<string, string>();
+                    originImagesVKList = new Dictionary<string, string>();
+                    repeatNum = 0;
+                    listFileOrFolder(new DirectoryInfo(folderPath));
+                    _syncContext.Post(checkButtonStatus, "");
+                });
+                task1.Start();
+            }
+        }
+
+        private void qqDefault_Click(object sender, EventArgs e)
+        {
+            folderPath = "C:\\Users\\zhouw\\Documents\\Tencent Files";
+            originImages.Items.Clear();
+            repeatImages.Items.Clear();
+            deleteProgressBar.Value = 0;
+            var task1 = new Task(() =>
+            {
+                repeatImageList = new Dictionary<string, List<string>>();
+                originImagesList = new Dictionary<string, string>();
+                originImagesVKList = new Dictionary<string, string>();
+                repeatNum = 0;
+                listFileOrFolder(new DirectoryInfo(folderPath));
+                _syncContext.Post(checkButtonStatus, "");
+            });
+            task1.Start();
+        }
+        private void listFileOrFolder(DirectoryInfo fileSystemInfo)
+        {
+            if (!fileSystemInfo.Exists)
+            {
+                return;
+            }
+            DirectoryInfo[] dir = fileSystemInfo.GetDirectories();
+            if (dir.Length != 0)
+            {
+                foreach (var item in dir)
+                {
+                    listFileOrFolder(new DirectoryInfo(item.FullName));
+                }
+            }
+            foreach (var item in fileSystemInfo.GetFileSystemInfos())
+            {
+                if (File.Exists(item.FullName))
+                {
+                    if (item.Extension == ".jpg" || item.Extension == ".gif" || item.Extension == ".png" || item.Extension == ".bmp")
+                    {
+                        string fileMd5 = GetMD5HashFromFile(item.FullName);
+                        if (originImagesList.ContainsKey(fileMd5))
+                        {
+                            if (repeatImageList.ContainsKey(fileMd5))
+                            {
+                                if (!repeatImageList[fileMd5].Contains(item.FullName))
+                                {
+                                    repeatImageList[fileMd5].Add(item.FullName);
+                                }
+
+                            }
+                            else
+                            {
+                                List<string> repeatImagesList = new List<string>();
+                                repeatImagesList.Add(item.FullName);
+                                repeatImageList.Add(fileMd5, repeatImagesList);
+                            }
+                            repeatNum++;
+                            _syncContext.Post(setRepeatNumLabel, "重复图片数量：" + repeatNum);
+                            _syncContext.Post(repeatImagesAddItem, item.FullName);
+                        }
+                        else
+                        {
+                            originImagesVKList.Add(item.FullName, fileMd5);
+                            originImagesList.Add(fileMd5, item.FullName);
+                            _syncContext.Post(originImagesAddItem, item.FullName);
+                        }
+
+                    }
+                }
+            }
+        }
+
+        private void originImagesAddItem(object path)
+        {
+            originImages.Items.Add(path.ToString());
+        }
+
+        private void setRepeatNumLabel(object text)
+        {
+            repeatNumLabel.Text = text.ToString();
+        }
+
+        private void repeatImagesAddItem(object path)
+        {
+            repeatImages.Items.Add(path.ToString());
+        }
+
+        public static string GetMD5HashFromFile(string fileName)
+        {
+            try
+            {
+                FileStream file = File.OpenRead(fileName);
+                if (!File.Exists(fileName))
+                {
+                    return "";
+                }
+                MD5 md5 = MD5.Create();
+                byte[] retVal = md5.ComputeHash(file);
+                file.Close();
+                file.Dispose();
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < retVal.Length; i++)
+                {
+                    sb.Append(retVal[i].ToString("x2"));
+                }
+                return sb.ToString();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("GetMD5HashFromFile() fail,error:" + ex.Message);
+            }
+        }
+
+        private void originImages_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (originImages.SelectedItem==null)
+            {
+                return;
+            }
+            originImage.Load(originImages.SelectedItem.ToString());
+            string imagemd5 = originImagesVKList[originImages.SelectedItem.ToString()];
+            if (repeatImageList.ContainsKey(imagemd5))
+            {
+                currentSelectedRepeatNum = 0;
+                selectedRepeatItemIndex = 0;
+                repeatImages.ClearSelected();
+                selectedRepeatItem = new List<object>();
+                foreach (var item in repeatImages.Items)
+                {
+                    if (repeatImageList[imagemd5].Contains(item.ToString()))
+                    {
+                        selectedRepeatItem.Add(item);
+                        currentSelectedRepeatNum++;
+                    }
+                }
+                if (selectedRepeatItem.Count == 0)
+                {
+                    repeatImage.Image = null;
+                    currentFileRepeat.Text = "当前图片重复数量：" + 0;
+                    MessageBox.Show("该图片没有重复");
+                    return;
+                }
+                checkButtonStatus("");
+                repeatImages.SelectedItem = selectedRepeatItem[0];
+                repeatImageBoxLoad(selectedRepeatItem[0].ToString());
+                currentFileRepeat.Text = "当前图片重复数量：" + currentSelectedRepeatNum;
+            }
+            else
+            {
+                repeatImage.Image = null;
+                currentFileRepeat.Text = "当前图片重复数量：" + 0;
+                MessageBox.Show("该图片没有重复");
+            }
+        }
+
+        private void deleteSelected_Click(object sender, EventArgs e)
+        {
+            if (selectedRepeatItem.Count == 0)
+            {
+                return;
+            }
+
+            selectedRepeatItem.RemoveAt(selectedRepeatItemIndex);
+            if (selectedRepeatItem.Count != 0)
+            {
+                selectedRepeatItemIndex = selectedRepeatItemIndex % selectedRepeatItem.Count;
+                repeatImageBoxLoad(selectedRepeatItem[selectedRepeatItemIndex % selectedRepeatItem.Count].ToString());
+            }
+            else
+            {
+                repeatImage.Image = null;
+            }
+            string image = repeatImages.SelectedItem.ToString();
+            if (File.Exists(image))
+            {
+                File.Delete(image);
+                repeatNum--;
+                repeatImages.Items.Remove(repeatImages.SelectedItem);
+                if (selectedRepeatItem.Count != 0)
+                    repeatImages.SelectedItem = selectedRepeatItem[selectedRepeatItemIndex % selectedRepeatItem.Count];
+                _syncContext.Post(setRepeatNumLabel, "重复图片数量：" + repeatNum);
+                currentFileRepeat.Text = "当前图片重复数量：" + selectedRepeatItem.Count;
+            }
+            if (Directory.Exists(image.Substring(0, image.LastIndexOf("\\"))) && new DirectoryInfo(image.Substring(0, image.LastIndexOf("\\"))).GetFileSystemInfos().Length == 0)
+            {
+                Directory.Delete(image.Substring(0, image.LastIndexOf("\\")));
+            }
+        }
+
+        private void deleteAll_Click(object sender, EventArgs e)
+        {
+
+            repeatImage.Image = null;
+            var task1 = new Task(() =>
+            {
+                double allRepeatNum = repeatNum;
+                double deleteNum = 0;
+                foreach (var md5 in originImagesList)
+                {
+                    if (repeatImageList.ContainsKey(md5.Key))
+                    {
+                        foreach (var image in repeatImageList[md5.Key])
+                        {
+                            if (File.Exists(image))
+                            {
+                                File.Delete(image);
+                                deleteNum++;
+                                repeatNum--;
+                                _syncContext.Post(setRepeatNumLabel, "重复图片数量：" + repeatNum);
+                                _syncContext.Post(setProgress, deleteNum / allRepeatNum * 100);
+                            }
+                            if (Directory.Exists(image.Substring(0, image.LastIndexOf("\\"))) && new DirectoryInfo(image.Substring(0, image.LastIndexOf("\\"))).GetFileSystemInfos().Length == 0)
+                            {
+                                Directory.Delete(image.Substring(0, image.LastIndexOf("\\")));
+                            }
+                        }
+                    }
+                }
+                _syncContext.Post(initRepeatListBox, deleteNum);
+            });
+            task1.Start();
+        }
+        private void setProgress(object progress)
+        {
+            deleteProgressBar.Value = Convert.ToInt32(progress);
+        }
+
+        private void initRepeatListBox(object deleteNum)
+        {
+            repeatImageList = new Dictionary<string, List<string>>();
+            originImagesList = new Dictionary<string, string>();
+            originImagesVKList = new Dictionary<string, string>();
+            originImages.Items.Clear();
+            repeatImages.Items.Clear();
+            repeatNum = 0;
+            deleteProgressBar.Value = 0;
+            listFileOrFolder(new DirectoryInfo(folderPath));
+            MessageBox.Show("删除成功 共" + deleteNum.ToString() + "张");
+        }
+
+        private void refreshCurrent_Click(object sender, EventArgs e)
+        {
+            repeatImageList = new Dictionary<string, List<string>>();
+            originImagesList = new Dictionary<string, string>();
+            originImagesVKList = new Dictionary<string, string>();
+            originImages.Items.Clear();
+            repeatImages.Items.Clear();
+            deleteProgressBar.Value = 0;
+            repeatNum = 0;
+            currentFileRepeat.Text = "当前图片重复数量：" + 0;
+            listFileOrFolder(new DirectoryInfo(folderPath));
+        }
+
+        private void next_Click(object sender, EventArgs e)
+        {
+            selectedRepeatItemIndex++;
+            repeatImageBoxLoad(selectedRepeatItem[selectedRepeatItemIndex % selectedRepeatItem.Count].ToString());
+            repeatImages.SelectedItem = selectedRepeatItem[selectedRepeatItemIndex % selectedRepeatItem.Count];
+        }
+
+        private void checkButtonStatus(object data)
+        {
+            if (string.IsNullOrEmpty(folderPath))
+            {
+                deleteSelected.Enabled = false;
+                deleteAll.Enabled = false;
+                next.Enabled = false;
+                deleteSelectedAll.Enabled = false;
+                refreshCurrent.Enabled = false;
+            }
+            else
+            {
+                deleteSelected.Enabled = true;
+                deleteAll.Enabled = true;
+                deleteSelectedAll.Enabled = true;
+                refreshCurrent.Enabled = true;
+            }
+
+            if (originImages.SelectedItem != null)
+            {
+                next.Enabled = true;
+            }
+        }
+
+        private void repeatImageBoxLoad(string filepath)
+        {
+            Image img = Image.FromFile(filepath);
+            Image bmp = new Bitmap(img);
+            repeatImage.Image = bmp;
+            img.Dispose();
+        }
+
+        private void deleteSelectedAll_Click(object sender, EventArgs e)
+        {
+            if (selectedRepeatItem.Count == 0)
+            {
+                return;
+            }
+            var task1 = new Task(() =>
+            {
+                double allRepeatNum = repeatNum;
+                double deleteNum = 0;
+                repeatImage.Image = null;
+                foreach (var item in selectedRepeatItem)
+                {
+                    if (File.Exists(item.ToString()))
+                    {
+                        File.Delete(item.ToString());
+                        deleteNum++;
+                        repeatNum--;
+                        _syncContext.Post(removeRepeatItems, "");
+                        _syncContext.Post(setRepeatNumLabel, "重复图片数量：" + repeatNum);
+                        currentFileRepeat.Text = "当前图片重复数量：" + selectedRepeatItem.Count;
+                        _syncContext.Post(setProgress, deleteNum / allRepeatNum * 100);
+                    }
+                    if (Directory.Exists(item.ToString().Substring(0, item.ToString().LastIndexOf("\\"))) && new DirectoryInfo(item.ToString().Substring(0, item.ToString().LastIndexOf("\\"))).GetFileSystemInfos().Length == 0)
+                    {
+                        Directory.Delete(item.ToString().Substring(0, item.ToString().LastIndexOf("\\")));
+                    }
+                }
+                selectedRepeatItem.Clear();
+                _syncContext.Post(refreshRepeatListBox, deleteNum);
+            });
+            task1.Start();
+        }
+        private void refreshRepeatListBox(object deleteNum)
+        {
+            repeatImageList = new Dictionary<string, List<string>>();
+            originImagesList = new Dictionary<string, string>();
+            originImagesVKList = new Dictionary<string, string>();
+            originImages.Items.Clear();
+            repeatImages.Items.Clear();
+            repeatNum = 0;
+            deleteProgressBar.Value = 0;
+            currentFileRepeat.Text = "当前图片重复数量：" + 0;
+            listFileOrFolder(new DirectoryInfo(folderPath));
+            MessageBox.Show("删除成功 共" + deleteNum.ToString() + "张");
+        }
+
+        private void removeRepeatItems(object data)
+        {
+            repeatImages.Items.Remove(repeatImages.SelectedItem);
+        }
+    }
+}
